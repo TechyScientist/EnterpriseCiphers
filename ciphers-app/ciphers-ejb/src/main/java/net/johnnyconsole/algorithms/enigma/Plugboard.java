1c9eda987da189b584608cc5e5cc6e5a5cfceca9
Johnny Console <johnny.console@hotmail.com>
2023-09-20 08:56:31 -0400

    Add Enigma Plugboard Class

---
+++ ciphers-app/ciphers-ejb/src/main/java/net/johnnyconsole/algorithms/enigma/Plugboard.java
@@ -0,0 +1,32 @@
+package net.johnnyconsole.algorithms.enigma;
+
+public class Plugboard {
+
+	public char[] left = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+	public char[] right = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+	
+	public Plugboard(String... pairs) {
+		for(String pair : pairs) {
+			int a = pair.charAt(0) - 'A', b = pair.charAt(1) - 'A';
+			char t = left[a];
+			left[a] = left[b];
+			left[b] = t;
+		}
+	}
+	
+	public int forward(int signal) {
+		return find(left, right[signal]);
+	}
+	
+	public int reverse(int signal) {
+		return find(right, left[signal]);
+	}
+	
+	private int find(char[] array, char key) {
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] == key) return i;
+		}
+		return -1;
+	}
+	
+}
