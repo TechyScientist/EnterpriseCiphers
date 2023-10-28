ae42fbee0b5345ef15e88b8ee5a7bd462604ab38
Johnny Console <johnny.console@hotmail.com>
2023-10-28 15:05:32 -0400

    Add Morse Codes, add Support for Numbers to Ohaver's Algorithm
    Signed-off-by: Johnny Console <johnny.console@hotmail.com>

--- ciphers-app/ciphers-ejb/src/main/java/net/johnnyconsole/beans/ohavers/OhaversAlgorithmSingleton.java
+++ ciphers-app/ciphers-ejb/src/main/java/net/johnnyconsole/beans/ohavers/OhaversAlgorithmSingleton.java
@@ -80,6 +80,26 @@ public class OhaversAlgorithmSingleton implements OhaversAlgorithmSingletonLocal
                 return "-.--";
             case 'Z':
                 return "--..";
+            case '1':
+                return ".----";
+            case '2':
+                return "..---";
+            case '3':
+                return "...--";
+            case '4':
+                return "....-";
+            case '5':
+                return ".....";
+            case '6':
+                return "-....";
+            case '7':
+                return "--...";
+            case '8':
+                return "---..";
+            case '9':
+                return "----.";
+            case '0':
+                return "-----";
             case '_':
                 return "..--";
             case ',':
@@ -156,6 +176,26 @@ public class OhaversAlgorithmSingleton implements OhaversAlgorithmSingletonLocal
                 return 'Y';
             case "--..":
                 return 'Z';
+            case ".----":
+                return '1';
+            case "..---":
+                return '2';
+            case "...--":
+                return '3';
+            case "....-":
+                return '4';
+            case ".....":
+                return '5';
+            case "-....":
+                return '6';
+            case "--...":
+                return '7';
+            case "---..":
+                return '8';
+            case "----.":
+                return '9';
+            case "-----":
+                return '0';
             case "..--":
                 return '_';
             case ".-.-":
