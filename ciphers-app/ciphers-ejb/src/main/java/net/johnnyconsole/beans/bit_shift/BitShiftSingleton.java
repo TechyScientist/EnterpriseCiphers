e5a47575d63d7607c85b4d6926c66480803d883f
Johnny Console <johnny.console@hotmail.com>
2024-05-26 17:24:58 -0400

    Add Bit Shift Servlet to web component

--- ciphers-app/ciphers-ejb/src/main/java/net/johnnyconsole/beans/bit_shift/BitShiftSingleton.java
+++ ciphers-app/ciphers-ejb/src/main/java/net/johnnyconsole/beans/bit_shift/BitShiftSingleton.java
@@ -11,32 +11,24 @@ public class BitShiftSingleton implements BitShiftLocal, BitShiftRemote {
     // TODO: FIX THIS!!
 	@Override
 	public String encipher(String message, boolean shift, int key) {
-        StringBuilder ciphertext = new StringBuilder(message);
+        StringBuilder ciphertext = new StringBuilder();
         for(int i = 0; i < message.length(); i++) {
             if(shift) {
-                ciphertext.replace(i, i+1, rol(message.charAt(i), key) + "");
+                ciphertext.append(rol(message.charAt(i), key));
             }
             else {
-                ciphertext.replace(i, i+1, ror(message.charAt(i), key) + "");
+                ciphertext.append(ror(message.charAt(i), key));
             }
         }
         return ciphertext.toString();
 	}
 
     private char ror(char c, int key) {
-        String s = Integer.toBinaryString((byte)c);
-        for(int i = 0; i < key; i++) {
-            s = s.charAt(s.length() - 1) + s.substring(0, s.length() - 1);
-        }
-        return (char)(Integer.parseInt(s, 2));
+        return (char)((((byte)c & 0xff) >>> key) | (((byte)c & 0xff) << (Integer.SIZE - key)));
     }
 
     private char rol(char c, int key) {
-        String s = Integer.toBinaryString((byte)c);
-        for(int i = 0; i < key; i++) {
-            s = s.substring(1) + s.charAt(0);
-        }
-        return (char)(Integer.parseInt(s, 2));
+        return (char)((((byte)c & 0xff) << key) | (((byte)c & 0xff) >>> (Integer.SIZE - key)));
     }
 
 
